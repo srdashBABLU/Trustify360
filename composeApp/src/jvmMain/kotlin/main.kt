6e0f44134c54d105fdc14ce6ebfd512ed60dba65
Chinmaya-001 <158756895+Chinmaya-001@users.noreply.github.com>
2025-07-20 19:26:51 +0530

    login screen called at main method but not working

--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -1,9 +1,11 @@
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.neurostax.trustify360.App
 import com.neurostax.trustify360.core.manager.navigation.AppNavHost
+import com.neurostax.trustify360.presentation.common.LoginScreen
 import com.neurostax.trustify360.presentation.common.PrivacyPolicyScreen
 import java.awt.Dimension
 
@@ -15,7 +17,18 @@ fun main() = application {
     ) {
         window.minimumSize = Dimension(350, 600)
 //        PrivacyPolicyScreen()
-        AppNavHost()
+        //AppNavHost()
+        LoginScreen(
+
+            onLoginClicked = {
+
+            } as (String, String) -> Unit,
+            onSignupClicked = {
+
+            }as (String, String) -> Unit,
+            Modifier
+        )
+
     }
 }
 
